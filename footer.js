028c3678d2ec476a792c5a56b6d5314346a10792
DEV WEB Paris <devops.abis@gmail.com>
2024-07-26 19:21:33 +0200

    add counter js viste to the website

--- footer.js
+++ footer.js
@@ -5,3 +5,28 @@ document.addEventListener('DOMContentLoaded', function() {
     const footerText = document.getElementById('footer-text');
     footerText.textContent = `© ${currentYear} Media List. All rights reserved.`;
 });
+
+
+//______________________________________________________________ //
+
+// Sélection de l'élément du DOM pour afficher le compteur
+const counterDisplay = document.getElementById('counter');
+
+// Fonction pour mettre à jour l'affichage du compteur
+function updateCounterDisplay(count) {
+    counterDisplay.textContent = count;
+}
+
+// Fonction pour incrémenter le compteur de visites
+function incrementVisitCounter() {
+    let visitCount = localStorage.getItem('visitCount');
+    if (visitCount === null) {
+        visitCount = 0;
+    } else {
+        visitCount = parseInt(visitCount, 10);
+    }
+    visitCount++;
+    localStorage.setItem('visitCount', visitCount);
+    updateCounterDisplay(visitCount);
+}
+incrementVisitCounter();
